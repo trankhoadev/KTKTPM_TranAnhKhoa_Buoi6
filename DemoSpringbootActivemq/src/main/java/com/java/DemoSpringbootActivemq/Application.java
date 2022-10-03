fcc3f2eb8575da3d8458ca092f6ad2eb2078107c
trankhoadev <trankhoadev@gmail.com>
2022-10-03 11:05:07 +0700

    add model class

---
+++ DemoSpringbootActivemq/src/main/java/com/java/DemoSpringbootActivemq/Application.java
@@ -0,0 +1,7 @@
+package com.java.DemoSpringbootActivemq;
+
+public class Application {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ DemoSpringbootActivemq/src/main/java/com/java/DemoSpringbootActivemq/SpringBootJwtApplication.java
@@ -0,0 +1,15 @@
+package com.java.DemoSpringbootActivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class SpringBootJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtApplication.class, args);
+    }
+
+}
