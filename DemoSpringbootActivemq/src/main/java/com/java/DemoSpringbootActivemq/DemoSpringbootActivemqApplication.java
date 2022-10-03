5e6a595b98765814b0b4963e68a13be2a58228c3
trankhoadev <trankhoadev@gmail.com>
2022-10-03 10:17:07 +0700

    Add blank project

---
+++ DemoSpringbootActivemq/src/main/java/com/java/DemoSpringbootActivemq/DemoSpringbootActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.java.DemoSpringbootActivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootActivemqApplication.class, args);
+	}
+
+}
---
+++ DemoSpringbootActivemq/src/test/java/com/java/DemoSpringbootActivemq/DemoSpringbootActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.DemoSpringbootActivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootActivemqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
